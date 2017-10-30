d86829b0c2e3e924f63261f2660f30fc9bc69edc
Joel, Pinto da Mata <joel.mata@iflavours.nl>
2017-10-30 09:23:29 +0100

    Controller tests

--- src/test/java/com/example/codeChallenge/controllers/MainControllerTests.java
+++ src/test/java/com/example/codeChallenge/controllers/MainControllerTests.java
@@ -2,18 +2,27 @@ package com.example.codeChallenge.controllers;
 
 import com.example.codeChallenge.CodeChallengeApplication;
 import com.example.codeChallenge.readers.SparkReader;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 @WebAppConfiguration
 @ContextHierarchy({
         @ContextConfiguration(classes = CodeChallengeApplication.class)
@@ -21,30 +30,22 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @TestPropertySource(locations = "classpath:/application-test.properties")
 public class MainControllerTests {
 
-	@Autowired
+    @Autowired
     @Qualifier("sparkJsonFileReader")
     SparkReader sparkReader;
 
-    @Test
-    public void contextLoads() {
-        Assert.assertEquals(sparkReader.getAll().size(), 898);
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = standaloneSetup(new MainController()).build();
     }
 
     @Test
-    public void actor1() {
-        Assert.assertEquals(sparkReader.getAll().size(), 898);
-    }
-
-    @Test
-    public void actor2() {
-        Assert.assertEquals(sparkReader.getAll().size(), 898);
-    }
-
-    /**
-     * The total sum of a contains plus not contains should be equal to the total of records in the rdd
-     */
-    @Test
-    public void containsPlusNotContains() {
-        Assert.assertEquals(sparkReader.getByContains("disney").size() + sparkReader.getByNotContains("disney").size(), sparkReader.getAll().size());
+    public void testSayHelloWorld() throws Exception {
+        this.mockMvc.perform(get("/")
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
     }
 }
