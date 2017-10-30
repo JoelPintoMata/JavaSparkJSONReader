300da177f89a8603f7e7188ff43ae2006f9373fa
Joel, Pinto da Mata <joel.mata@iflavours.nl>
2017-10-30 09:10:42 +0100

    Spark specific tests creation

---
+++ src/test/java/com/example/codeChallenge/readers/SparkJsonFileReaderTests.java
@@ -0,0 +1,43 @@
+package com.example.codeChallenge.readers;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:/application-test.properties")
+public class SparkJsonFileReaderTests {
+
+	@Autowired
+    @Qualifier("sparkJsonFileReader")
+    SparkReader sparkReader;
+
+    @Test
+    public void contextLoads() {
+        Assert.assertEquals(sparkReader.getAll().size(), 898);
+    }
+
+    @Test
+    public void actor1() {
+        Assert.assertEquals(sparkReader.getAll().size(), 898);
+    }
+
+    @Test
+    public void actor2() {
+        Assert.assertEquals(sparkReader.getAll().size(), 898);
+    }
+
+    /**
+     * The total sum of a contains plus not contains should be equal to the total of records in the rdd
+     */
+    @Test
+    public void containsPlusNotContains() {
+        Assert.assertEquals(sparkReader.getByContains("disney").size() + sparkReader.getByNotContains("disney").size(), sparkReader.getAll().size());
+    }
+}
