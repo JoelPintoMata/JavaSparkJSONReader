83dbae17b3a877b833bfbec5fc57e80fd370f963
Joel, Pinto da Mata <joel.mata@iflavours.nl>
2017-10-30 08:35:59 +0100

    Implementation of a dedicated application test properties file

--- src/test/java/com/example/codeChallenge/CodeChallengeApplicationTests.java
+++ src/test/java/com/example/codeChallenge/CodeChallengeApplicationTests.java
@@ -5,13 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@PropertySources(value = {@PropertySource("classpath:/application-test.properties")})
+@TestPropertySource(locations = "classpath:/application-test.properties")
 public class CodeChallengeApplicationTests {
 
 	@Autowired
